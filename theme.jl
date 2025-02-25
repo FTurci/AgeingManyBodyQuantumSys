74888954bbaab96c32eb63aeda412281eacd9bc7
FTurci <turci@me.com>
2025-02-25 09:23:15 +0000

    First commit

---
+++ theme.jl
@@ -0,0 +1,13 @@
+Attributes(
+    fontsize = 11,
+    size = (500,300),
+    inline = true,
+    # backgroundcolor = :gray,
+    color = :blue,
+    linestyle = :dot,
+    linewidth = 1,
+    Axis = (
+        xgridvisible = false,
+        ygridvisible = false,
+    ),
+)
