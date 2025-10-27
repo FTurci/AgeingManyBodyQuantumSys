a3a6cee28a815656ba93312f07fc2c9f251116ba
Ben Bray <hm22978@bristol.ac.uk>
2025-10-27 16:59:44 +0000

    exact solution implemented

--- Ben Code/OQS_Tools_v1.jl
+++ Ben Code/OQS_Tools_v1.jl
@@ -5,6 +5,7 @@ module OQS_Tools_v1
     using LinearAlgebra
     using PolyChaos
     using QuadGK
+    using Plots
 
     export create_spectral
     export modify_spectral
@@ -12,17 +13,20 @@ module OQS_Tools_v1
     export chain_map
     export make_H_mpo
     export make_H_matrix
+    export prepare_MPS
     export evolve_MPS
+    export prepare_correlations
+    export evolve_correlations  
+
 
     function create_spectral(input::AbstractString, D::Float64, g::Float64) # (bool, amp, mean, sd)
         # window
         inband = x -> (-D <= x <= D)
-        # gaussian term
-        G = gaussian ? (x -> amp * (1/(sd*sqrt(2pi))) * exp(-0.5*((x-mean)/sd)^2)) : (x -> 0.0)
+        
         if input == "flat"
-            J = x -> inband(x) ? 1/(2D) + G(x) : 0.0
+            J = x -> inband(x) ? 1/(2D) : 0.0
         elseif input == "elliptical"
-            J = x -> inband(x) ? sqrt(1 - (x/D)^2)+ G(x) : 0.0
+            J = x -> inband(x) ? sqrt(1 - (x/D)^2) : 0.0
         else
             error("spectral function type not recognized")
         end
@@ -38,9 +42,9 @@ module OQS_Tools_v1
         return Jmod
     end
 
-    function thermofield_transform(J::Function, beta::Float64, mu::Float64) #spectral function, inverse temp, chemical potential
+    function thermofield_transform(J, beta::Float64, mu::Float64) #spectral function, inverse temp, chemical potential
         """thermofield purification using fermi function ancilla"""
-        fermi = k -> 1/(1 + exp(beta*k - beta*mu))
+        fermi(k) = 1/(1 + exp(beta*k - beta*mu))
         J1 = w -> J(w) * fermi(w) #filled mode spectral density
         J2 = w -> J(w) * (1 - fermi(w)) #empty mode spectral density
         return J1, J2
@@ -53,9 +57,19 @@ module OQS_Tools_v1
         meas = Measure("bath", J, supp, false, Dict())
         ortho_poly = OrthoPoly("bath_op", N, meas; Nquad=2000)   
         chain = coeffs(ortho_poly)                                  
-        E = chain[:,1] #site energies
-        h = sqrt.(chain[1:N+1,2]) #site hoppings (first term is system hopping)
+        E = chain[1:N,1] #site energies
+        h = sqrt.(chain[1:N,2]) #site hoppings (first term is system hopping)
         return E, h
+    end
+
+    function prepare_MPS(N::Int64, sys_occ::Float64)
+        """prepare initial MPS state, currently just for fermions"""
+        sites = siteinds("Fermion", 2*N+1, conserve_qns=true) #assuming that truncated bath remains approximately closed within simulation time
+
+        states = [(j <= N) ? "Occ" : "Emp" for j=1:2*N+1]
+
+        psi0 = productMPS(sites, states)
+        return psi0, sites, states
     end
 
     function make_H_mpo(E1, E2, h1, h2, sites, Es, N, sys::Int64) #filled/empty site energies, filled/empty hoppings, ITensor sites, system onsite energy, chain length, system site index
@@ -88,21 +102,19 @@ module OQS_Tools_v1
         return MPO(ampo, sites)
     end
 
-    function make_H_exact(E1, E2, h1, h2, Es) #filled/empty site energies, filled/empty hoppings, chain length, system site index
+    function make_H_matrix(E1, E2, h1, h2, Es, N::Int64) #filled/empty site energies, filled/empty hoppings, chain length, system site index
         """exact diagonalisation Hamiltonian for chain mapped OQS"""
         E1 = reverse(E1) #empty chain onsite energies
         h1 = reverse(h1) #empty chain NN couplings
-        L = length(E1)
-        d = Vector{Float64}(undef, 2L+1)     # diagonal 
-        e = Vector{Float64}(undef, 2L)       # off-diagonal
-        d[1:L] .= E1
-        d[L+1] = Es
-        d[L+2:2L+1] .= E2
-        e[1:L] .= h1
-        e[L+1:2L] .= h2
+        d = Vector{Float64}(undef, 2*N+1)     # diagonal 
+        e = Vector{Float64}(undef, 2*N)       # off-diagonal
+        d[1:N] .= E1
+        d[N+1] = Es
+        d[N+2:2N+1] .= E2
+        e[1:N] .= h1
+        e[N+1:2N] .= h2
         H = SymTridiagonal(d, e)            # Hermitian and tridiagonal
-        idx = (d = 1, b = collect(2:L+1))
-        return H, idx
+        return H
 
     end
 
@@ -113,31 +125,30 @@ module OQS_Tools_v1
         ts = collect(dt:dt:tmax)
         len = length(ts)
         nSys = zeros(len)
-        num = zeros(2N+1)
         for k in 1:len
             psi = tdvp(H, -im*dt, psi; nsite=2, outputlevel=0,mindim=1, maxdim=100) #time_step=dt, nsweeps=sweeps, order=2)
-            nSys[k] = expect(psi, "N", sys)
+            nSys[k] = expect(psi, "N")[sys]
             println("timestep $k of $len complete")
         end
         return psi, nSys
     end
 
-    function make_correlations(N, sys_occ)                             
+    function prepare_correlations(N, sys_occ)                             
         nd0 = sys_occ                 
-        # block helpers
-        I_L  = I(N)                # identity on filled chain
+        
+        I_L  = I(N)                
         Z_L  = zeros(N, N)
 
         # assemble C0
-        C0 = zeros(ComplexF64, N, N)
+        C0 = zeros(ComplexF64, 2N+1, 2N+1)
         C0[N+1,N+1] = nd0 + 0im
-        C0[1:N, 1:N] .= I_L    # filled chain
-        C0[2+N:end, 2+N:end] .= Z_L# empty chain
+        C0[1:N, 1:N] .= I_L    
+        C0[2+N:end, 2+N:end] .= Z_L
         C0 = Hermitian(C0)
         return C0
     end
 
-    function evolve_correlations(C, dt, tmax, H)
+    function evolve_correlations(C, H, dt, tmax, N::Int64)
         ts = collect(dt:dt:tmax)
         F = eigen(H)
         U = Matrix(F.vectors)
@@ -149,6 +160,6 @@ module OQS_Tools_v1
             C = U * C * U'
             nSys[k] = real(C[N+1,N+1])
         end
-        return nSys, C
+        return C, nSys
     end
-end
+end #module
